6d3b6cbf50a35e203103c444ab25f9f9e8f7df91
Stephen Shank <sshank314@gmail.com>
2020-03-11 11:16:05 -0400

    Custom component, amino acid alignment.

--- app.js
+++ app.js
@@ -1,25 +1,91 @@
 import React, { useState, useEffect } from "react";
-import ReactDOM from "react-dom";
 import Phylotree from "react-phylotree";
-import TreeAlignment from "alignment.js/TreeAlignment";
+import ReactDOM from "react-dom";
+import ScrollBroadcaster from "alignment.js/helpers/ScrollBroadcaster";
+import { nucleotide_color } from "alignment.js/helpers/colors";
+import BaseAlignment from "alignment.js/components/BaseAlignment";
+import Placeholder from "alignment.js/components/Placeholder";
+import SiteAxis from "alignment.js/components/SiteAxis";
 import StopWobbling from "alignment.js/prevent_default_patch";
 import { fnaParser } from "alignment.js/helpers/fasta";
-import Navbar from "react-bootstrap/Navbar";
 import * as d3 from "d3";
 
 import "./styles.scss";
 
+function molecule(mol) {
+  return mol;
+};
+
 function Visualization(props) {
   if(!props.data) return <div />;
   const { absrel, gard, fna } = props.data,
-    parsedFna = fnaParser(fna, true);
-  return (<div>
-    <TreeAlignment
-      {...parsedFna}
-      width={1200}
-      height={500}
-    />
-  </div>);
+    { sequence_data, tree } = fnaParser(fna, true),
+    width = 2400,
+    tree_width = 1200,
+    height = 500,
+    axis_height = 25,
+    site_size = 20,
+    padding = 10,
+    full_pixel_width = sequence_data
+      ? sequence_data[0].seq.length * site_size
+      : null,
+    full_pixel_height = sequence_data ? sequence_data.length * site_size : null,
+    alignment_width = full_pixel_width
+      ? Math.min(full_pixel_width, width - tree_width)
+      : width,
+    alignment_height = full_pixel_height
+      ? Math.min(full_pixel_height, height - axis_height)
+      : height,
+    scroll_broadcaster = new ScrollBroadcaster({
+      width: full_pixel_width,
+      height: full_pixel_height,
+      x_pad: width - tree_width,
+      y_pad: height - axis_height,
+      bidirectional: [
+        "alignmentjs-alignment",
+        "alignmentjs-axis-div"
+      ]
+    });
+  return (
+    <div
+      style={{
+        display: "grid",
+        gridTemplateRows: `${axis_height}px ${alignment_height}px`,
+        gridTemplateColumns: `${tree_width}px ${alignment_width}px`
+      }}
+    >
+      <Placeholder width={tree_width} height={axis_height} />
+      <SiteAxis
+        width={alignment_width}
+        height={axis_height}
+        sequence_data={sequence_data}
+        scroll_broadcaster={scroll_broadcaster}
+      />
+      <div>
+        <svg width={tree_width} height={alignment_height}>
+          <g transform={`translate(${padding}, ${padding})`}>
+            <Phylotree
+              tree={tree}
+              width={tree_width - 2*padding}
+              height={alignment_height - 2*padding}
+              maxLabelWidth={100}
+              alignTips="right"
+            />
+          </g>
+        </svg>
+      </div>
+      <BaseAlignment
+        sequence_data={sequence_data}
+        width={alignment_width}
+        height={alignment_height}
+        site_size={site_size}
+        site_color={nucleotide_color}
+        scroll_broadcaster={scroll_broadcaster}
+        molecule={molecule}
+        amino_acid
+      />
+    </div>
+  );
 }
 
 function App() {
