90c212fcddc9fbc6ccb89f4ce50bf8664404a4d3
stephenshank <sshank314@gmail.com>
2020-03-17 14:18:30 -0400

    Single page app architecture for multiple datasets.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch, useRouteMatch } from "react-router-dom";
 import Visualization from "./viz.js";
 import * as d3 from "d3";
 import StopWobbling from "alignment.js/prevent_default_patch";
@@ -7,14 +8,15 @@ import StopWobbling from "alignment.js/prevent_default_patch";
 import "./styles.scss";
 
 
-function App() {
-  const [data, setData] = useState(null);
+function Fetcher(props) {
+  const [data, setData] = useState(null),
+    { dataset } = props;
   useEffect(() => {
     Promise.all([
-      d3.json('output/S.fna.FUBAR.json'),
-      d3.text('output/S-full.fasta'),
+      d3.json(`output/${dataset}.fna.FUBAR.json`),
+      d3.text(`output/${dataset}-full.fasta`),
       d3.text('input/pdb6vxx.ent'),
-      d3.csv('output/S-map.csv')
+      d3.csv(`output/${dataset}-map.csv`)
     ]).then(data => {
       setData({
         fubar: data[0],
@@ -28,6 +30,27 @@ function App() {
     <h1>HyPhy Coronavirus Evolution</h1>
     {data ? <Visualization data={data} /> : null}
   </div>);
+}
+
+function Wrapper(props) {
+  const match = useRouteMatch(),
+    { dataset } = match.params;
+  return <Fetcher dataset={dataset} />;
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/:dataset">
+          <Wrapper />
+        </Route>
+        <Route path="/">
+          <Fetcher dataset="S" />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 StopWobbling(document)
